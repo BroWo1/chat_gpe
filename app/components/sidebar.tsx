f60174c3a6c81a438bf44c46d588aa74c6e3021b
BroWo1 <zhangyang.li@icloud.com>
2024-12-01 20:12:33 +0800

    comment

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -307,7 +307,6 @@ export function SideBar(props: { className?: string }) {
               </Link>
             </div>
             <div className={styles["sidebar-action"]}>
-              <!--<a href={REPO_URL} target="_blank" rel="noopener noreferrer">-->
               <a href={`https://github.com/BroWo1`} target="_blank" rel="noopener noreferrer">
                 <IconButton
                   aria={Locale.Export.MessageFromChatGPT}
